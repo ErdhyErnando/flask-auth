16bbb1f3e506c90454decfead6e0560d30bbcec3
Erdhy Ernando <ernandodavis1002@gmail.com>
2024-06-20 21:11:59 +0100

    add basic chart (test file worked)

---
+++ orthosis-scripts/hello.py
@@ -0,0 +1,13 @@
+#print('Hello world!')
+
+#Learning from Automate the Boring task w/ python
+
+'''
+name = ''
+while name != 'your name':
+    print('please type your name')
+name = input()
+print('thank you')    
+'''
+
+print('Brian\'s mother: He\'s not an angel. He\'s a very naughty boy')
